5d20cb6cbc4ba227f709fe4edb1f24e1490eaf0c
AmandaBalderas <amanda.balderas@comunidad.unam.mx]>
2024-11-02 22:23:26 -0600

    Uploading page.jsx

--- fimarket/app/page.jsx
+++ fimarket/app/page.jsx
@@ -1,5 +1,14 @@
+import SignIn from './components/SignIn';
+/*
 export default function Home() {
   return (
     <></>
   );
 }
+*/
+
+const SignInPage = () => {
+  return <SignIn/>;
+};
+
+export default SignInPage;
