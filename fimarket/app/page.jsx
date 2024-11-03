eb50420983f8eeed34bc7df594c7968d49ceebff
Ghero56 <arcigaguzmanfernando@gmail.com>
2024-11-03 00:18:06 -0600

    Feat: apps landing page

--- fimarket/app/page.jsx
+++ fimarket/app/page.jsx
@@ -1,5 +1,30 @@
-export default function Home() {
+'use client';
+
+import React, { useState } from 'react';
+import { CssBaseline, Box, Container } from '@mui/material';
+import AppList from './components/AppList';
+import SearchBar from './components/SearchBar';
+import BackgroundContainer from './components/BackgroundContainer';
+
+const Page = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filterDialogOpen, setFilterDialogOpen] = useState(false);
+
+  const handleFilterClick = () => {
+    setFilterDialogOpen(true);
+  };
+
   return (
-    <></>
+    <BackgroundContainer>
+      <CssBaseline />
+      <Container maxWidth="md" sx={{ pt: 4 }}>
+        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} onFilterClick={handleFilterClick} />
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <AppList searchTerm={searchTerm} filterDialogOpen={filterDialogOpen} setFilterDialogOpen={setFilterDialogOpen} />
+        </Box>
+      </Container>
+    </BackgroundContainer>
   );
-}
+};
+
+export default Page;
