fca176ecee8e23fa50f7d7dc074a683638815c42
Arturo Francisco <arturofco26@comunidad.unam.mx>
2024-11-03 23:35:58 -0600

    fix: improve code styling and formatting

--- fimarket/app/about/page.jsx
+++ fimarket/app/about/page.jsx
@@ -5,11 +5,11 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import SchoolIcon from '@mui/icons-material/School'; 
-import GroupIcon from '@mui/icons-material/Group'; 
-import PeopleIcon from '@mui/icons-material/People'; 
-import StarIcon from '@mui/icons-material/Star'; 
-import FlagIcon from '@mui/icons-material/Flag'; 
+import SchoolIcon from "@mui/icons-material/School"; 
+import GroupIcon from "@mui/icons-material/Group"; 
+import PeopleIcon from "@mui/icons-material/People"; 
+import StarIcon from "@mui/icons-material/Star"; 
+import FlagIcon from "@mui/icons-material/Flag"; 
 
 export default function About() {
     return (
@@ -174,11 +174,11 @@ export default function About() {
                             display="flex" 
                             alignItems="center" 
                             sx={{ 
-                                border: '1px solid #000000', 
+                                border: "1px solid #000000", 
                                 borderRadius: 4, 
                                 padding: 3, 
                                 mb: 2, 
-                                backgroundColor: '#cc0000'
+                                backgroundColor: "#cc0000"
                             }}
                         >
                             <SchoolIcon sx={{ mr: 2, color: "#ffffff" }} />
@@ -191,11 +191,11 @@ export default function About() {
                             display="flex" 
                             alignItems="center" 
                             sx={{ 
-                                border: '1px solid #000000', 
+                                border: "1px solid #000000", 
                                 borderRadius: 4, 
                                 padding: 3, 
                                 mb: 2, 
-                                backgroundColor: '#cc0000'
+                                backgroundColor: "#cc0000"
                             }}
                         >
                             <GroupIcon sx={{ mr: 2, color: "#ffffff" }} />
@@ -208,11 +208,11 @@ export default function About() {
                             display="flex" 
                             alignItems="center" 
                             sx={{ 
-                                border: '1px solid #000000', 
+                                border: "1px solid #000000", 
                                 borderRadius: 4, 
                                 padding: 3, 
                                 mb: 2, 
-                                backgroundColor: '#cc0000'
+                                backgroundColor: "#cc0000"
                             }}
                         >
                             <PeopleIcon sx={{ mr: 2, color: "#ffffff" }} />
@@ -225,16 +225,16 @@ export default function About() {
                             display="flex" 
                             alignItems="center" 
                             sx={{ 
-                                border: '1px solid #000000', 
+                                border: "1px solid #000000", 
                                 borderRadius: 4, 
                                 padding: 3, 
                                 mb: 2, 
-                                backgroundColor: '#cc0000'
+                                backgroundColor: "#cc0000"
                             }}
                         >
                             <FlagIcon sx={{ mr: 2, color: "#ffffff" }} />
                             <Typography variant="body1" sx={{ color: "#ffffff" }}>
-                                Social Impact: We are committed to promoting projects that generate a positive impact on society and improve people's quality of life.
+                                Social Impact: We are committed to promoting projects that generate a positive impact on society and improve people"s quality of life.
                             </Typography>
                         </Box>
 
@@ -242,11 +242,11 @@ export default function About() {
                             display="flex" 
                             alignItems="center" 
                             sx={{ 
-                                border: '1px solid #000000', 
+                                border: "1px solid #000000", 
                                 borderRadius: 4, 
                                 padding: 3, 
                                 mb: 2, 
-                                backgroundColor: '#cc0000'
+                                backgroundColor: "#cc0000"
                             }}
                         >
                             <StarIcon sx={{ mr: 2, color: "#ffffff" }} />
